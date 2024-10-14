26063dc3ba78d140a731e0f46031b69473f30490
JuanOsu <juanosunamelgarejo@gmail.com>
2024-10-14 19:41:12 +0200

    EjercicioOrder

---
+++ src/Model/CustomerType.java
@@ -0,0 +1,8 @@
+package Model;
+//enum son los tipos de clientes
+public enum CustomerType {
+	 REGULAR,
+	    VIP
+	
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+import Model.CustomerType;
+import Model.Order;
+
+public class Main {
+	 public static void main(String[] args) {
+	        List<String> items = new ArrayList<>();
+	        items.add("Artículo1");
+	        items.add("Artículo2");
+
+	        Order order = new Order("Alice", CustomerType.VIP, items, 1200);
+	        order.applyDiscount();
+	        order.calculateTax();
+	        order.printOrderDetails();
+	    }
+
+}
